9e9bbfbc9927ca7af0c3c230af87f4e6d7f9859c
tdbuu <tdbuu@cmcglobal.vn>
2022-07-15 15:25:09 +0700

    First

---
+++ vue-crud-version1/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header-bar></header-bar>
+    <b-container>
+      <b-row class="mt-5">
+        <data-table></data-table>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import HeaderBar from "@/components/HeaderBar.vue"; 
+import DataTable from "@/components/DataTable.vue";
+
+export default {
+  name: 'App',
+  components: {
+    HeaderBar,
+    DataTable
+  }
+}
+</script>
+
